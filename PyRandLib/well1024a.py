e75255a1e14801309347693d2d3af85022b6ed75
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-02-09 12:08:48 +0100

    #39-Implement algorithm Well1024a
    Completed.

---
+++ PyRandLib/well1024a.py
@@ -0,0 +1,130 @@
+"""
+Copyright (c) 2025 Philippe Schmouker, schmouk (at) gmail.com
+
+Permission is hereby granted,  free of charge,  to any person obtaining a copy
+of this software and associated documentation files (the "Software"),  to deal
+in the Software without restriction, including  without  limitation the rights
+to use,  copy,  modify,  merge,  publish,  distribute, sublicense, and/or sell
+copies of the Software,  and  to  permit  persons  to  whom  the  Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS",  WITHOUT WARRANTY OF ANY  KIND,  EXPRESS  OR
+IMPLIED,  INCLUDING  BUT  NOT  LIMITED  TO  THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT  SHALL  THE
+AUTHORS  OR  COPYRIGHT  HOLDERS  BE  LIABLE  FOR  ANY CLAIM,  DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  TORT OR OTHERWISE, ARISING FROM,
+OUT  OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+#=============================================================================
+from .basewell import BaseWELL
+
+#=============================================================================
+class Well1024a( BaseWELL ):
+    """
+    Pseudo-random numbers generator - Definition of a fast  32-bits  Well-Equilibrated 
+    Long-period Linear generator with a large period (2^512, i.e. 1.34e+154).
+    This module is part of library PyRandLib.
+        
+    Copyright (c) 2025 Philippe Schmouker
+
+    Well-Equilibrated Long-period Linear Generators (WELL) use linear recurrence based 
+    on  primitive  characteristic  polynomials associated with left- and right- shifts 
+    and xor operations to fastly evaluate pseudo-random numbers suites.
+    
+    WELLs offer large to very large periods with best known results in the  evaluation 
+    of their randomness,  as stated in the evaluation  done  by  Pierre  L'Ecuyer  and 
+    Richard Simard (Universite de Montreal) in  "TestU01:  A C Library  for  Empirical 
+    Testing of Random  Number Generators  - ACM Transactions on Mathematical Software, 
+    vol.33 n.4, pp.22-40, August 2007".  It is recommended to use  such  pseudo-random
+    numbers generators rather than LCG ones for serious simulation applications.
+    Furthermore, WELLs have proven their great ability  to  very  fastly  escape  from 
+    zeroland.
+
+    Notice: the algorithm in its Well1024a version has been coded  here  as  a  direct 
+    implementation  of  their  descriptions in the initial paper "Improved Long-Period
+    Generators Based on Linear Recurrences Modulo 2",  François  PANNETON  and  Pierre 
+    L’ECUYER (Université de Montréal) and MAKOTO MATSUMOTO (Hiroshima University),  in
+    ACM Transactions on Mathematical Software, Vol. 32, No. 1, March 2006, Pages 1–16.
+    (see https://www.iro.umontreal.ca/~lecuyer/myftp/papers/wellrng.pdf).
+    So,  only minimalist optimization has been coded,  with  the  aim  at  easing  the 
+    verification of its proper implementation.
+       
+    See Well512a for a large period WELL-Generator (2^512,  i.e. 1.34e+154)  with  low
+    computation time and 16 integers memory little consumption.
+    See Well1024a for a longer period WELL-Generator  (2^1024,  i.e. 2.68e+308),  same 
+    computation time and 32 integers memory consumption.
+    See Well199937b for a far longer period  (2^19937,  i.e. 4.32e+6001) with  similar 
+    computation time but use of more memory space (624 integers).
+    See Well44497c for a very large period (2^44497,  i.e. 15.1e+13466)  with  similar 
+    computation time but use of even more memory space (1,391 integers).
+    
+    Furthermore, this class is callable:
+      rand = Well1024a()
+      print( rand() )    # prints a pseudo-random value within [0.0, 1.0)
+      print( rand(a) )   # prints a pseudo-random value within [0.0, a)
+      print( rand(a,b) ) # prints a pseudo-random value within [a  , b)
+    
+    Notice that for simulating the roll of a dice you should program:
+      diceRoll = MRGRand287()
+      print( int(diceRoll(1, 7)) ) # prints a uniform roll within set {1, 2, 3, 4, 5, 6}
+
+    Such a programming is an accelerated while still robust emulation of the inherited 
+    methods:
+      - random.Random.randint(self,1,6) and 
+      - random.Random.randrange(self,1,7,1)
+
+    Reminder:
+    We give you here below a copy of the table of tests for the WELL  algorithms  that 
+    have  been implemented in PyRandLib,  as provided in paper "TestU01, ..." and when 
+    available.
+
+ | PyRabndLib class | TU01 generator name | Memory Usage    | Period  | time-32bits | time-64 bits | SmallCrush fails | Crush fails | BigCrush fails |
+ | ---------------- | ------------------- | --------------- | ------- | ----------- | ------------ | ---------------- | ----------- | -------------- |
+ | Well512a         | not available       |    16 x 4-bytes | 2^512   |    n.a.     |     n.a.     |        n.a.      |     n.a.    |     n.a.       |
+ | Well1024a        | WELL1024a           |    32 x 4-bytes | 2^1024  |    4.0      |     1.1      |          0       |       4     |       4        |
+ | Well19937b (1)   | WELL19937a          |   624 x 4-bytes | 2^19937 |    4.3      |     1.3      |          0       |       2     |       2        |
+ | Well44497c       | not available       | 1,391 x 4-bytes | 2^44497 |    n.a.     |     n.a.     |        n.a.      |     n.a.    |     n.a.       |
+
+    (1)The Well19937b generator provided with library PyRandLib implements the
+    Well19937a algorithm augmented with an associated tempering algorithm.
+
+    * _small crush_ is a small set of simple tests that quickly tests some  of
+    the expected characteristics for a pretty good PRG;
+    * _crush_ is a bigger set of tests that test more deeply  expected  random 
+    characteristics;
+    * _big crush_ is the ultimate set of difficult tests  that  any  GOOD  PRG 
+    should definitively pass.
+    """
+        
+    #-------------------------------------------------------------------------
+    # 'protected' constant
+    _LIST_SIZE = 16     # this Well1024a PRNG is based on a suite containing 16 integers
+            
+ 
+    #-------------------------------------------------------------------------
+    def random(self) -> float:
+        """This is the core of the pseudo-random generator.
+        
+        Returned values are within [0.0, 1.0).
+        """
+        i = self._index
+        i_1 = (i - 1) & 0x1f
+
+        z0 = self._list[i_1]  # notice:  all blocks of bits in the internal state are 32 bits wide, which leads
+                              # to a great simplification for the implementation of the generic WELL algorithm.
+        z1 = self._list[i] ^ self._M3_pos(self._list[(i + 3) & 0x1f], 8)
+        z2 = self._M3_neg(self._list[(i + 24) & 0x1f], 19) ^ self._M3_neg(self._list[(i + 10) & 0x1f], 14)
+        
+        self._list[i] = z1 ^ z2
+        self._list[i_1] = self._M3_neg(z0, 11) ^ self._M3_neg(z1, 7) ^ self._M3_neg(z2, 13)
+
+        self._index = i_1
+        return self._list[i] * 2.328_306_436_538_696_289_062_5e-10
+
+
+#=====   end of module   well1024a.py   ======================================
