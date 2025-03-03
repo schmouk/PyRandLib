fffed3cc3698ce8f729354c961b1e2cf9bcb9824
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-03-03 23:47:25 +0100

    #114-rename initial PyRandLib directory with Py...3.6
    Created a new directory instead. Completed. Validated.

---
+++ Python3.6/PyRandLib/annotation_types.py
@@ -0,0 +1,35 @@
+"""
+Copyright (c) 2021-2025 Philippe Schmouker, schmouk (at) gmail.com
+
+Permission is hereby granted,  free of charge,  to any person obtaining a copy
+of this software and associated documentation files (the "Software"),  to deal
+in the Software without restriction, including  without  limitation the rights
+to use,  copy,  modify,  merge,  publish,  distribute, sublicense, and/or sell
+copies of the Software,  and  to  permit  persons  to  whom  the  Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS",  WITHOUT WARRANTY OF ANY  KIND,  EXPRESS  OR
+IMPLIED,  INCLUDING  BUT  NOT  LIMITED  TO  THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT  SHALL  THE
+AUTHORS  OR  COPYRIGHT  HOLDERS  BE  LIABLE  FOR  ANY CLAIM,  DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  TORT OR OTHERWISE, ARISING FROM,
+OUT  OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+#=============================================================================
+from typing import List, Tuple, Union
+
+Numerical        = Union[ int, float ]
+StatesList       = Union[ Tuple[int], List[int] ]
+StatesListAndExt = Union[ Tuple[ StatesList, int ], Tuple[ StatesList, int, int ] ]
+StateType        = Union[ StatesList, StatesListAndExt ]
+SeedStateType    = Union[ Numerical, StateType ]
+
+
+#=====   end of   PyRandLib.annotation_types   ===============================
+
+# type: ignore  (this comment line is just to avoid boring pylance related error checking)
