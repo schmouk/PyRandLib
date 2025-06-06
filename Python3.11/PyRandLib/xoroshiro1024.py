e153946c356e16606b8324540e6892abf41d76b4
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-06-06 23:13:17 +0200

    #157-fix errors in docstrings
    Fixed erroneous email address.

--- Python3.11/PyRandLib/xoroshiro1024.py
+++ Python3.11/PyRandLib/xoroshiro1024.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2025 Philippe Schmouker, schmouk (at) gmail.com
+Copyright (c) 2025 Philippe Schmouker, ph (dot) schmouker (at) gmail.com
 
 Permission is hereby granted,  free of charge,  to any person obtaining a copy
 of this software and associated documentation files (the "Software"),  to deal
