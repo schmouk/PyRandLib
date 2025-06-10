69a03155d29a91f4b03b94c6a16868064ad8e6bd
Philippe Schmouker <ph.schmouker@yahoo.fr>
2025-06-10 04:44:36 +0200

    #201-test class BasePCG
    Fully completed.

---
+++ Python3.13/unit_tests/test_basepcg.py
@@ -0,0 +1,72 @@
+"""
+Copyright (c) 2025 Philippe Schmouker, ph (dot) schmouker (at) gmail.com
+
+Permission is hereby granted,  free of charge,  to any person obtaining a copy
+of this software and associated documentation files (the "Software"),  to deal
+in the Software without restriction, including  without  limitation the rights
+to use,  copy,  modify,  merge,  publish,  distribute, sublicense, and/or sell
+copies of the Software,  and  to  permit  persons  to  whom  the  Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS",  WITHOUT WARRANTY OF ANY  KIND,  EXPRESS  OR
+IMPLIED,  INCLUDING  BUT  NOT  LIMITED  TO  THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT  SHALL  THE
+AUTHORS  OR  COPYRIGHT  HOLDERS  BE  LIABLE  FOR  ANY CLAIM,  DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  TORT OR OTHERWISE, ARISING FROM,
+OUT  OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+#=============================================================================
+import pytest
+
+from PyRandLib.basepcg import BasePCG
+
+
+#=============================================================================
+class TestBasePCG:
+    """Tests the base class BasePCG"""
+    
+    #-------------------------------------------------------------------------
+    def test_init_empty(self):
+        b_pcg = BasePCG()
+        assert b_pcg.gauss_next is None
+
+    #-------------------------------------------------------------------------
+    def test_init_int(self):
+        b_pcg = BasePCG(0X1234_5678_9abc_def0)
+        assert b_pcg.gauss_next is None
+
+    #-------------------------------------------------------------------------
+    def test_init_float(self):
+        b_pcg = BasePCG(0.1)
+        assert b_pcg.gauss_next is None
+        
+    #-------------------------------------------------------------------------
+    def test_init_tuple(self):
+        with pytest.raises(TypeError):
+            b_pcg = BasePCG((0, 1, 0X1234_5678_9abc_def0, 0X1234_5678_9abc_def0))
+                
+    #-------------------------------------------------------------------------
+    def test_init_list(self):
+        with pytest.raises(TypeError):
+            b_pcg = BasePCG([0, 1, 0X1234_5678_9abc_def0, 0X1234_5678_9abc_def0])
+                
+    #-------------------------------------------------------------------------
+    def test_init_tuple_int(self):
+        with pytest.raises(TypeError):
+            b_pcg = BasePCG( tuple((0, 1, 0X1234_5678_9abc_def0, 0X1234_5678_9abc_def0), 11) )
+
+    #-------------------------------------------------------------------------
+    def test_init_list_int(self):
+        with pytest.raises(TypeError):
+            b_pcg = BasePCG( ([0, 1, 0X1234_5678_9abc_def0, 0X1234_5678_9abc_def0], 11))
+                
+    #-------------------------------------------------------------------------
+    def test_getstate(self):
+        b_pcg = BasePCG()
+        with pytest.raises(AttributeError):
+            state = b_pcg.getstate()
